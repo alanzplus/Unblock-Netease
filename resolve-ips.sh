4b5331246bb9b84c122fe90b25677ebae4941ad8
Alan Xuan <jzxuanuni@gmail.com>
2019-04-14 15:44:29 +0900

    Uncomment script and update resolved ips

--- resolve-ips.sh
+++ resolve-ips.sh
@@ -6,13 +6,13 @@ if [[ $1 == "rebuild" ]]; then
   ./gradlew clean install
 fi
 
-#readonly domainsToLookup="p1.music.126.net p2.music.126.net p3.music.126.net p4.music.126.net m10.music.126.net img3.126.net"
-#JAVA_OPTS="
-#-Dmusic163DomainName=music.163.com
-#-DunblockYouKuIp=158.69.209.100
-#-Ddns.query.concurrency=10
-#-Ddns.query.timeoutIntSeconds=10
-#-Dping.concurrency=30
-#-Dping.timeoutInSeconds=10
-#-Dout.hostfile=./resolved-ip-host
-#" ./build/install/Unblock-Netease/bin/Unblock-Netease ${domainsToLookup}
+readonly domainsToLookup="p1.music.126.net p2.music.126.net p3.music.126.net p4.music.126.net m10.music.126.net img3.126.net"
+JAVA_OPTS="
+-Dmusic163DomainName=music.163.com
+-DunblockYouKuIp=158.69.209.100
+-Ddns.query.concurrency=10
+-Ddns.query.timeoutIntSeconds=10
+-Dping.concurrency=30
+-Dping.timeoutInSeconds=10
+-Dout.hostfile=./resolved-ip-host
+" ./build/install/Unblock-Netease/bin/Unblock-Netease ${domainsToLookup}
